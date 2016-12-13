5a2ccb0da91860a3179ba049437d33297833d7d7
TaikiFnit <taikifnitspg@gmail.com>
2016-12-13 13:36:31 +0900

    fix bug of view and kaizen process when tapped equal

--- Fnit-Calculator/ViewController.swift
+++ Fnit-Calculator/ViewController.swift
@@ -10,19 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var harfResultLabel: UILabel!
-    
     @IBOutlet weak var resultLabel: UILabel!
     
     var leftOperand:String? = ""
     var rightOperand:String? = ""
     var centerOperator:String = ""
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    var is_end:Bool = false
     
     /* 演算子が押されたときの処理 */
     @IBAction func tappedOperator(_ sender: UIButton) {
@@ -32,22 +26,44 @@ class ViewController: UIViewController {
             // 左辺の値を確定し、
             leftOperand = resultLabel.text!
             // 次の値を受け付ける
+            rightOperand = ""
 
             resultLabel.text = ""
         }
-        
-        //calculate()
+        // すでに確定している場合で、operatorもsetしてあったら
+        else if (centerOperator != "") {
+            /*
+            print("do calc")
+            // ラベルに表示されている値を右辺として変数に代入
+            rightOperand = resultLabel.text
+
+            // 計算させる関数
+            calculate()
+            
+            // calculate()を実行した結果がresultLabelに表示されるので、それをさらにleftOperandに登録して,後に演算子を登録
+            leftOperand = resultLabel.text
+            
+            // 数値の受付
+            resultLabel.text = ""
+            */
+        }
         
         // pushされた演算子を変数に格納
         if let ope: String = sender.currentTitle {
             centerOperator = ope
-            
-            harfResultLabel.text = leftOperand! + " " + centerOperator + " " +  rightOperand!
         }
+        
+        is_end = false
+        refresh()
     }
 
     /* 数字が押されたときの処理 */
     @IBAction func tappedNum(_ sender: UIButton) {
+        
+        if is_end == true {
+            resultLabel.text = ""
+            harfResultLabel.text = ""
+        }
         
         // 押された値がしっかり取得できていれば、それを表示させる
         if let num: String = sender.currentTitle {
@@ -57,27 +73,36 @@ class ViewController: UIViewController {
             }
             // labelに押された値を表示
             resultLabel.text = resultLabel.text! + num
-            
-            // 途中結果を表示させる
-            harfResultLabel.text = leftOperand! + " " + centerOperator + " " +  rightOperand!
         }
+        
+        is_end = false
+        refresh()
     }
     
     // ドットが押されたときの処理
     @IBAction func tappedDot(_ sender: UIButton) {
         
-        resultLabel.text = resultLabel.text! + "."
+        if !hasDot(origString: resultLabel.text!) {
+            resultLabel.text = resultLabel.text! + "."
+            
+            is_end = false
+            refresh()
+        }
     }
-    
+
     // =が押されたときの処理
     @IBAction func tappedEqual(_ sender: UIButton) {
-        
-        // ラベルに表示されている値を右辺として変数に代入
-        rightOperand = resultLabel.text
-        // 途中経過を表示
-        harfResultLabel.text = leftOperand! + " " + centerOperator + " " +  rightOperand!
-        // 計算させる関数
-        calculate()
+        if is_end == false {
+            // ラベルに表示されている値を右辺として変数に代入
+            rightOperand = resultLabel.text
+            // 途中経過を表示
+            harfResultLabel.text = leftOperand! + " " + centerOperator + " " +  rightOperand!
+            // 計算させる関数
+            calculate()
+            // 計算の終了
+            is_end = true
+        }
+        refresh()
     }
     
     // クリアボタンが押されたときの処理
@@ -87,6 +112,9 @@ class ViewController: UIViewController {
         centerOperator = ""
         resultLabel.text = "0"
         harfResultLabel.text = "0"
+        
+        refresh()
+        is_end = false
     }
     
     
@@ -101,8 +129,10 @@ class ViewController: UIViewController {
         if resultLabel.text == "" {
             resultLabel.text = "0"
         }
+        
+        is_end = false
+        refresh()
     }
-    
     
     func calculate() {
         
@@ -133,6 +163,25 @@ class ViewController: UIViewController {
             rightOperand = ""
             centerOperator = ""
         }
+    }
+    
+    func refresh() {
+        harfResultLabel.text = leftOperand! + " " + centerOperator + " " +  rightOperand!
+    }
+    
+    func hasDot(origString:String)->Bool{
+        
+        for i in origString.characters.indices[origString.startIndex..<origString.endIndex] {
+            if (origString[i] == ".") {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
